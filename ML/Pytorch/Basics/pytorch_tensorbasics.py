462d1b6adc7de043abe49443534b79fa62735005
JYH <jYuhao123@163.com>
2022-07-13 22:16:49 +0800

    Initial commit

---
+++ ML/Pytorch/Basics/pytorch_tensorbasics.py
@@ -0,0 +1,16 @@
+"""
+Walk through of a lot of different useful Tensor Operations, where we
+go through what I think are four main parts in:
+
+1. Initialization of a Tensor
+2. Tensor Mathematical Operations and Comparison
+3. Tensor Indexing
+4. Tensor Reshaping
+
+But also other things such as setting the device (GPU/CPU) and converting
+between different types (int, float etc) and how to convert a tensor to an
+numpy array and vice-versa.
+
+Programmed by Aladdin Persson
+* 2022-07-13: Initial coding
+"""
