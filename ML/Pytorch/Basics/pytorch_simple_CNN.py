b2c2680cee1acfe822b3aafde3331bae569ec310
JYH <jYuhao123@163.com>
2022-07-17 15:24:06 +0800

    Initial commit

--- ML/Pytorch/Basics/pytorch_simple_CNN.py
+++ ML/Pytorch/Basics/pytorch_simple_CNN.py
@@ -1,5 +1,5 @@
 """
-A simple walkthrough of how to code a fully connected neural network
+A simple walkthrough of how to code a convolutional neural network (CNN)
 using the PyTorch library. For demonstration we train it on the very
 common MNIST dataset of handwritten digits. In this code we go through
 how to create the network as well as initialize a loss function, optimizer,
@@ -8,9 +8,8 @@ check accuracy and more.
 Programmed by Aladdin Persson
 Translated by RehoboamX
 
-* 2022-07-15: Initial coding
-* 2022-07-15: Added more detailed comments also removed part of
-              check_accuracy which would only work specifically on MNIST.
+* 2020-07-17: Initial coding
+* 2021-07-17: More detailed comments and small revision of the code
 """
 
 # Imports导入即将被用到的包
@@ -25,49 +24,56 @@ from torch.utils.data import DataLoader  # Gives easier dataset management by cr
 from tqdm import tqdm  # For nice progress bar!
 
 
-# 在这部分我们创建一个普通的全连接神经网络，下面我们定义继承自父类nn.Module的子类NN
-# 这是构建神经网络一种最常用的方式并且有很大的灵活性，有时我们还需要使用nn.Sequential让神经网络的构建变得更容易
-class NN(nn.Module):
-    def __init__(self, input_size, num_classes):
-        super(NN, self).__init__()
-        # 第一个全连接层fc1接收参数input_size，这里为784
-        # 第二个全连接层fc2接收fc1的输出作为输入，输出num_classes维，这里为10
-        self.fc1 = nn.Linear(input_size, 50)
-        self.fc2 = nn.Linear(50, num_classes)
+# 创建一个普通的卷积神经网络CNN
+class CNN(nn.Module):
+    def __init__(self, in_channels=1, num_classes=10):
+        super(CNN, self).__init__()
+        self.conv1 = nn.Conv2d(
+            in_channels=in_channels,
+            out_channels=8,
+            kernel_size=(3,3),
+            stride=(1,1),
+            padding=(1,1)
+        )
+        self.pool = nn.MaxPool2d(kernel_size=(2,2), stride=(2,2))
+        self.conv2 = nn.Conv2d(
+            in_channels=8,
+            out_channels=16,
+            kernel_size=(3, 3),
+            stride=(1, 1),
+            padding=(1, 1)
+        )
+        self.fc1 = nn.Linear(16*7*7, num_classes)
 
     def forward(self, x):
-        """
-        这里的 x指的是 Mnist输入图像，我们将x输入上面定义的 fc1，fc2
-        我们还在其中添加了非线性激活函数 Relu（其没有参数）
-        作者建议 Relu使用 nn.functional (F)里的函数来定义
-        """
-        x = F.relu(self.fc1(x))
-        x = self.fc2(x)
+        x = F.relu(self.conv1(x))
+        x = self.pool(x)
+        x = F.relu(self.conv2(x))
+        x = self.pool(x)
+        x = x.reshape(x.shape[0], -1)
+        x = self.fc1(x)
+
         return x
 
-#model = NN(784, 10)         # Mnist输入图像尺寸为28*28=784
-#x = torch.randn(64, 784)    # 64为设置的batchsize
-#print(model(x).shape)
 
-# 设置device，有GPU时在GPU上运行，否则在CPU上运行
+# 设置device
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
-# 神经网络超参数的具体设置和具体使用的数据集有关
-# 一些超参数也可以通过实验（超参数调优）去寻找比较理想的值，如学习率lr
-input_size = 784
+# 设置超参数
+in_channels = 1
 num_classes = 10
 learning_rate = 0.001
 batch_size = 64
-num_epochs = 1
+num_epochs = 5
 
 # 加载训练集和测试集
 train_dataset = datasets.MNIST(root='dataset/', train=True, transform=transforms.ToTensor(), download=True)
-train_loader = DataLoader(dataset=train_dataset, batch_size=batch_size, shuffle=True)
 test_dataset = datasets.MNIST(root='dataset/', train=False, transform=transforms.ToTensor(), download=True)
+train_loader = DataLoader(dataset=train_dataset, batch_size=batch_size, shuffle=True)
 test_loader = DataLoader(dataset=test_dataset, batch_size=batch_size, shuffle=False)
 
 # 初始化网络
-model = NN(input_size=input_size, num_classes=num_classes).to(device)
+model = CNN(in_channels=in_channels, num_classes=num_classes).to(device)
 
 # 损失函数（Loss）和优化器（optimizer）
 criterion = nn.CrossEntropyLoss()
@@ -79,9 +85,6 @@ for epoch in range(num_epochs):
         # 如果有GPU将数据放在cuda内
         data = data.to(device)
         targets = targets.to(device)
-
-        # 将数据转换为和网络输入相符合的形状
-        data = data.reshape(data.shape[0], -1)
 
         # 前向传播（forward）
         scores = model(data)
@@ -104,7 +107,6 @@ def check_accuracy(loader, model):
         for x, y in loader:
             x = x.to(device=device)
             y = y.to(device=device)
-            x = x.reshape(x.shape[0], -1)
 
             scores = model(x)
             _, predictions = scores.max(1)
